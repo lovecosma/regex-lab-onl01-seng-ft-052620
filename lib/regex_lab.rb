673144bb939aa183b5d2d751d77a294e1688153f
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-11 00:56:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,29 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+response = word.match(/\b[aeiou]\w+|\b[AEIOU]\w+/)
+if response == nil
+  return false
+else
+  return true
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+response = text.scan(/\b[un]\w+[ing]\b/)
 end
 
 def words_five_letters_long(text)
-
+response = text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  response = text.match(/[A-Z]\w+|[A-Z]\s+\b|\S/)
+  binding.pry
+  if response == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
